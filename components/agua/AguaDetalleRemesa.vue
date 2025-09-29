cabe0bc8d4dba832366ac265ac1da921ccb8817a
juankypunk <juanky.moral@gmail.com>
2025-09-29 13:29:06 +0200

    initial commit

--- components/agua/AguaDetalleRemesa.vue
+++ components/agua/AguaDetalleRemesa.vue
@@ -42,19 +42,45 @@
             <div>T3: {{ parseFloat((props.rowdata.pvpm3 * props.rowdata.f_c).toFixed(3)) }}€</div>
           </div>
           <div class="py-2">
-            Importe de los tramos:
+            Desglose del consumo:
           </div>
-          <div class="flex justify-start gap-6">
-            <div>T1: {{ parseFloat(props.rowdata.p_m3_a).toFixed(2) }}€ </div>
-            <div>T2: {{ parseFloat(props.rowdata.p_m3_b).toFixed(2) }}€</div>
-            <div>T3: {{ parseFloat(props.rowdata.p_m3_c).toFixed(2) }}€</div>
-          </div>
-            <div class="divider"></div>
+        
+        <table class="table table-sm table-zebra w-full">
+          <thead>
+            <tr>
+              <th>Tramo</th>
+              <th>m3</th>
+              <th>Precio</th>
+              <th>Importe</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>T1 (0-125m3):</td>
+              <td>{{ props.rowdata.m3_t1 }} m3</td>
+              <td>{{ parseFloat((props.rowdata.pvpm3 * props.rowdata.f_a)).toFixed(3) }}€</td>
+              <td align="right">{{ parseFloat((props.rowdata.p_m3_a)).toFixed(2) }}€</td>
+            </tr>
+            <tr v-if="props.rowdata.m3_t2 > 0">
+              <td>T2 (126-200m3):</td>
+              <td>{{ props.rowdata.m3_t2 }} m3</td>
+              <td>{{ parseFloat((props.rowdata.pvpm3 * props.rowdata.f_b)).toFixed(3) }}€</td>
+              <td align="right">{{ parseFloat((props.rowdata.p_m3_b)).toFixed(2) }}€</td>
+            </tr>
+            <tr v-if="props.rowdata.m3_t3 > 0">
+              <td>T3 (>200m3):</td>
+              <td>{{ props.rowdata.m3_t3 }} m3</td>
+              <td>{{ parseFloat((props.rowdata.pvpm3 * props.rowdata.f_c)).toFixed(3) }}€</td>
+              <td align="right">{{ parseFloat((props.rowdata.p_m3_c)).toFixed(2) }}€</td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="divider"></div>
             <div class="flex justify-end gap-2 py-2">
                 <div>Subtotal: {{props.rowdata.subtotal }}</div>
             </div>
             <div class="flex justify-end gap-2 py-2">
-                <div>IVA (10%): {{props.rowdata.impuesto }}</div>
+                <div>IVA ({{ props.rowdata.iva }}%): {{props.rowdata.impuesto }}</div>
             </div>
             <div class="flex justify-end gap-2">
                 <div>Total: {{props.rowdata.importe }}</div>
