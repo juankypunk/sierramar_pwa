a3a1002a26fe5d3aecea9cb222515240a805f039
juankypunk <juanky.moral@gmail.com>
2025-10-10 12:15:24 +0200

    initial commit

--- pages/agua/consumo/[id].vue
+++ pages/agua/consumo/[id].vue
@@ -76,13 +76,20 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 </script>
 
 <template>
-<div class="container mx-auto">
+  <div class="container mx-auto">
     <div role="tablist" class="tabs tabs-bordered">
-      <input type="radio" name="my_tabs_1" role="tab" class="tab" :aria-label="'Última lectura: ' + currentRemittance.fecha" checked />
+      <input
+        type="radio"
+        name="my_tabs_1"
+        role="tab"
+        class="tab"
+        :aria-label="'Última lectura: ' + currentRemittance.fecha"
+        checked
+      />
       <div role="tabpanel" class="tab-content py-5">
         <div class="stats shadow w-full">
           <div v-if="currentRemittance" class="stats stats-vertical shadow-xl">
-            <div  class="stat w-auto">
+            <div class="stat w-auto">
               <div class="stat-title flex justify-between">
                 <div>
                   Lect. anterior: {{ currentRemittance.l1 }} m3
@@ -92,10 +99,12 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
                 </div>
               </div>
               <div class="stat-value flex justify-center py-5">
-                <span class="px-3 text-4xl text-blue-600">{{currentRemittance.m3}} m3</span>
+                <span class="px-3 text-4xl text-blue-600">
+                  {{ currentRemittance.m3 }} m3
+                </span>
               </div>
               <div v-if="currentRemittance.m3" class="flex justify-center">
-                <AguaIndicador  
+                <AguaIndicador
                   :m3="parseInt(currentRemittance.m3)"
                   :t1="parseInt(currentRemittance.t1)"
                   :t2="parseInt(currentRemittance.t2)"
@@ -103,68 +112,127 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
               </div>
               <div class="stat-desc py-5">
                 <div class="flex justify-center gap-5">
-                  <div>T1(0-125m3): {{ currentRemittance.m3_t1 }}m3 </div>
-                  <div>T2(126-200m3): {{ currentRemittance.m3_t2 }}m3</div>
-                  <div>T3(+200): {{ currentRemittance.m3_t3 }}m3</div>
+                  <div>
+                    T1(0-125m3): {{ currentRemittance.m3_t1 }}m3
+                  </div>
+                  <div>
+                    T2(126-200m3): {{ currentRemittance.m3_t2 }}m3
+                  </div>
+                  <div>
+                    T3(+200): {{ currentRemittance.m3_t3 }}m3
+                  </div>
                 </div>
               </div>
             </div>
-            
+
             <div class="stat py-5">
               <div class="stat-title">Importe</div>
               <div class="stat-value flex justify-center">
-                <div class="px-3 text-4xl text-gray-600">{{currentRemittance.importe}} €</div>
+                <div class="px-3 text-4xl text-gray-600">
+                  {{ currentRemittance.importe }} €
+                </div>
               </div>
               <div class="stat-desc py-10">
                 <div>
-                    Precio del agua por tramos (IVA 10% incl.):
-                </div>
-                <div class="flex justify-center gap-2 px-5">
-                  <div>T1: {{ currentRemittance.pvpm3 }}€ </div>
-                  <div>T2: {{ (currentRemittance.pvpm3 * currentRemittance.f_b).toFixed(3) }}€</div>
-                  <div>T3: {{ (currentRemittance.pvpm3 * currentRemittance.f_c).toFixed(3) }}€</div>
+                  Desglose del consumo:
                 </div>
                 <div class="py-5">
-                  <div>
-                    Importe de los tramos:                
+                  <table class="table table-sm table-zebra w-full">
+                    <thead>
+                      <tr>
+                        <th>Tramo</th>
+                        <th>m3</th>
+                        <th>Precio</th>
+                        <th>Importe</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>T1 (0-125m3):</td>
+                        <td>{{ currentRemittance.m3_t1 }} m3</td>
+                        <td>
+                          {{ (currentRemittance.pvpm3 * currentRemittance.f_a).toFixed(3) }}€
+                        </td>
+                        <td align="right">
+                          {{ parseFloat(currentRemittance.p_m3_a).toFixed(2) }}€
+                        </td>
+                      </tr>
+                      <tr v-if="currentRemittance.m3_t2 > 0">
+                        <td>T2 (126-200m3):</td>
+                        <td>{{ currentRemittance.m3_t2 }} m3</td>
+                        <td>
+                          {{ (currentRemittance.pvpm3 * currentRemittance.f_b).toFixed(3) }}€
+                        </td>
+                        <td align="right">
+                          {{ parseFloat(currentRemittance.p_m3_b).toFixed(2) }}€
+                        </td>
+                      </tr>
+                      <tr v-if="currentRemittance.m3_t3 > 0">
+                        <td>T3 (&gt;200m3):</td>
+                        <td>{{ currentRemittance.m3_t3 }} m3</td>
+                        <td>
+                          {{ (currentRemittance.pvpm3 * currentRemittance.f_c).toFixed(3) }}€
+                        </td>
+                        <td align="right">
+                          {{ parseFloat(currentRemittance.p_m3_c).toFixed(2) }}€
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div class="divider"></div>
+                  <div class="flex justify-end gap-2 py-2">
+                    <div>
+                      Subtotal: {{ parseFloat(currentRemittance.subtotal).toFixed(2) }}€
+                    </div>
                   </div>
-                  <div class="flex justify-center gap-4 px-5">
-                    <div>T1: {{ parseFloat(currentRemittance.p_m3_a).toFixed(2) }}€ </div>
-                    <div>T2: {{ parseFloat(currentRemittance.p_m3_b).toFixed(2) }}€</div>
-                    <div>T3: {{ parseFloat(currentRemittance.p_m3_c).toFixed(2) }}€</div>
-                    <div class="px-8">Total: {{ parseFloat(currentRemittance.importe).toFixed(2) }}€</div>
+                  <div class="flex justify-end gap-2 py-2">
+                    <div>
+                      IVA ({{ currentRemittance.iva }}%): {{ parseFloat(currentRemittance.impuesto).toFixed(2) }}€
+                    </div>
                   </div>
-                  
+                  <div class="flex justify-end gap-2">
+                    <div>
+                      Total: {{ parseFloat(currentRemittance.importe).toFixed(2) }}€
+                    </div>
+                  </div>
                 </div>
-                <div>Titular: {{currentRemittance.titular}}</div>
+                <div>
+                  Titular: {{ currentRemittance.titular }}
+                </div>
                 <div>
                   <div v-if="currentRemittance.iban">
-                    Domiciliado en su cuenta terminada en: {{currentRemittance.iban.substr(20).padStart(10,'*')}}                
+                    Domiciliado en cuenta terminada en:
+                    {{ currentRemittance.iban.substr(20).padStart(10, '*') }}
                   </div>
-                </div>      
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
 
-      <input type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="Historial"/>
+      <input
+        type="radio"
+        name="my_tabs_1"
+        role="tab"
+        class="tab"
+        aria-label="Historial"
+      />
       <div role="tabpanel" class="tab-content py-10">
-          <div class="stats shadow w-full">
-              <MyGraph v-if="userParcel"
-                :id_parcela="userParcel"
-              />
-          </div>
-          <div>
-            <details class="collapse collapse-arrow">
-                <summary class="collapse-title text-xl font-medium">Historial de consumo (click para ver)</summary>
-                <div v-if="userParcel" class="collapse-content"> 
-                 <AguaHistorial :id_parcela="userParcel"/>
-                </div>
-            </details>
-          </div>
-          
-      </div>    
+        <div class="stats shadow w-full">
+          <MyGraph v-if="userParcel" :id_parcela="userParcel" />
+        </div>
+        <div>
+          <details class="collapse collapse-arrow">
+            <summary class="collapse-title text-xl font-medium">
+              Historial de consumo (click para ver)
+            </summary>
+            <div v-if="userParcel" class="collapse-content">
+              <AguaHistorial :id_parcela="userParcel" />
+            </div>
+          </details>
+        </div>
+      </div>
     </div>
   </div>
 </template>
