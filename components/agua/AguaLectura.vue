6e16bd5cd6d71fa9f0ba29eb63ff40d891d39eb2
juankypunk <juanky.moral@gmail.com>
2025-11-20 10:18:58 +0100

    initial commit

--- components/agua/AguaLectura.vue
+++ components/agua/AguaLectura.vue
@@ -208,20 +208,28 @@ function configMeter(){
 
 <template>
     <div class="container mx-auto">
-        <div class="">
-            <div class="flex justify-center">
+        <div class="py-5">
+            <div class="flex justify-center px-5 py-5 gap-10">
+                <div v-if="props.navegable">
+                    <span v-if="currentReadingForm.orden > 1" class="" @click="goBack()">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+                        </svg>
+                    </span>
+                </div>
                 <h3 class="font-bold text-2xl">Lectura parcela {{ currentReadingForm.id_parcela }}</h3>
+                <div v-if="props.navegable">
+                    <span  v-if="currentReadingForm.orden < readings.length" @click="goForward()">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                        </svg>
+                    </span>
+                </div>
             </div>
             <div class="stats shadow flex justify-center w-full">
                 <div class="stat">
                     <div class="flex justify-center gap-5">
-                        <div v-if="props.navegable">
-                            <span v-if="currentReadingForm.orden > 1" class="" @click="goBack()">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
-                                </svg>
-                            </span>
-                        </div>
+                        
                         <span v-if="currentReadingForm.estado ==='A'" @click="configMeter()">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
@@ -240,16 +248,8 @@ function configMeter(){
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                 </svg>
-
-                        </span>
-                        <div v-if="props.navegable">
-                            <span  v-if="currentReadingForm.orden < readings.length" @click="goForward()">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
-                                </svg>
-                            </span>
-                        </div>   
-                    </div>                
+                        </span>            
+                    </div>
                     <div class="py-5 stat-value flex justify-center">
                         <div class="grid gap-10 grid-cols-2">
                             <div class="text-4xl text-gray-600">
@@ -259,7 +259,7 @@ function configMeter(){
                         </div>        
                     </div>
                     <div class="stat-desc">
-                        <div class="flex justify-center gap-2 py-3">
+                        <div class="flex justify-center gap-2">
                             <div class="px-5">Lectura anterior</div>
                             <div class="px-5">Lectura actual</div>
 
@@ -278,7 +278,7 @@ function configMeter(){
                 />
             </div>
             <div class="stats shadow flex justify-center">
-                <div v-if="currentReadingForm.estado !='C'" class="stat flex justify-center">
+                <div class="stat flex justify-center">
                         <FormKit 
                         type="form" 
                         @submit="setCurrentReadings" 
@@ -297,17 +297,16 @@ function configMeter(){
                         validation-visibility="live"
                         validation="required|isValidL2"
                         :validation-rules="{isValidL2}"
-                        :validation-messages="{
-                            isValidL2: 'Lectura actual inferior a la anterior'
-                        }"
-                        autofocus
+                        :validation-messages="{isValidL2: 'Lectura actual inferior a la anterior'}"
+                        :disabled="currentReadingForm.estado === 'C'"
+                        autofocus="true"
                         />
                         <FormKit 
                         type="number" 
                         label="m3" 
                         name="m3" 
                         number
-                        :disabled="!m3_input"
+                        :disabled="!m3_input || currentReadingForm.estado === 'C'"
                         :help="'(Su media de consumo en el mismo trimestre es de '+currentReadingForm.avg+'m3)'"
                         />
                         <div class="flex justify-between">
