c6d253914a270c1c9ae3543f0287549179dee5f8
juankypunk <juanky.moral@gmail.com>
2026-02-21 20:23:40 +0100

    nightmode controller

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -64,8 +64,21 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
                     </button>
                 </div>
                 <div class="navbar-end">
+                    <label class="swap swap-rotate">
+                        <!-- this hidden checkbox controls the state -->
+                        <input type="checkbox" class="theme-controller" value="dark" />
+                        <!-- sun icon -->
+                        <svg class="swap-off h-10 w-10 size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                        </svg>
+                        <!-- moon icon -->
+                        <svg class="swap-on h-10 w-10 size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                        </svg>
+                    </label>
                     <div class="dropdown dropdown-end">
                             <div tabindex="0"   class="m-1 btn btn-ghost">
+                            
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                             </svg>
@@ -85,13 +98,7 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
                                     Cerrar sesión
                                 </span>
                             </li>
-                                <div class="divider">Preferencias</div>
-                            <div class="form-control">
-                                <label class="label cursor-pointer">
-                                <span class="label-text">Modo noche:</span> 
-                                    <input type="checkbox" id="myNightNodeCheck" name="nightMode"  v-model="nightMode" class="toggle"/>
-                                </label>
-                            </div>               
+                            <div class="divider">Preferencias</div>             
                             <div class="form-control">
                                 <label class="label cursor-pointer">
                                 <span class="label-text">Recuérdame:</span> 
